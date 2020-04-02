769f25b45f9309d35f7ba853bdc60690d7a23159
Vadim Aculov <vadim.aculov@gmail.com>
2020-04-02 12:33:10 +0300

    HW19-Add templates for links in header

--- src/js/components/header/header.js
+++ src/js/components/header/header.js
@@ -3,7 +3,32 @@ import headerTemplate from '../../templates/header.hbs';
 const headerContainer = document.getElementById('header-hbs');
 /* logic with nav links */
 
-var navigationLinks = [];
+var navigationLinks = [
+  {
+    url: '/',
+    label: 'Home',
+  },
+  {
+    url: '/about',
+    label: 'About Us',
+  },
+  {
+    url: '/plp',
+    label: 'Products',
+  },
+  {
+    url: '/contact',
+    label: 'Contacts',
+  },
+];
+
+navigationLinks = navigationLinks.map((navigationItem) => {
+  const item = navigationItem;
+  item.isActive = (window.location.pathname === navigationItem.url);
+
+  return item;
+});
+
 if (headerContainer) {
   headerContainer.innerHTML = headerTemplate({ navigationLinks });
 }
